b97074c6974a2a96c1ce2bc4c92d23e27f1e445e
clenio <mouraaf@hotmail.com>
2026-06-02 15:17:06 -0300

    fix: Usa variável de ambiente para a API_BASE_URL no frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ import PsicologoDashboard from './components/PsicologoDashboard';
 import PacienteDashboard from './components/PacienteDashboard';
 import './index.css';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 function App() {
   // Autenticação e Perfis
